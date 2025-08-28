4013866dd99d2855ae714c11ba51406613a303ee
Anis Khalfaoui <anis.khalfaoui@punitech.de>
2025-08-29 00:03:13 +0100

    dockerized all the services

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -30,6 +30,11 @@ internal static class HostingExtensions
                 options.Events.RaiseFailureEvents = true;
                 options.Events.RaiseSuccessEvents = true;
 
+                if (builder.Environment.IsEnvironment("Docker"))
+                {
+                    options.IssuerUri = "http://localhost:5001";
+                }
+
                 // Use a large chunk size for diagnostic data in development where it will be redirected to a local file.
                 if (builder.Environment.IsDevelopment())
                 {
@@ -64,7 +69,7 @@ internal static class HostingExtensions
 
         app.UseStaticFiles();
         app.UseRouting();
-        
+
         app.UseIdentityServer();
         app.UseAuthorization();
 
--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IdentityService.Pages.Account.Register;
+namespace IdentityService.Pages.Register;
 
 [SecurityHeaders]
 [AllowAnonymous]
