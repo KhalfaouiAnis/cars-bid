b8b9d584623988675ddcebcccf12d9f283141eaa
Anis Khalfaoui <anis.khalfaoui@punitech.de>
2025-08-28 10:02:38 +0100

    added rabbitmq event bus

---
+++ src/SearchService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using Contracts;
+using SearchService.Models;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles(){
+        CreateMap<AuctionCreated, Item>();
+        CreateMap<AuctionUpdated, Item>();
+    }
+}
